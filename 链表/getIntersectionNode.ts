bf76b8651ed8f76304dfe6f6f2fb42224236404c
sunsunmonkey <2933201543@qq.com>
2023-10-24 19:19:22 +0800

    feat: getIntersectionNode

---
+++ 链表/getIntersectionNode.ts
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function (headA, headB) {
+  let ALength = 0;
+  let BLength = 0;
+  let tempA = headA;
+  let tempB = headB;
+  while (tempA) {
+    tempA = tempA.next;
+    ALength++;
+  }
+  while (tempB) {
+    tempB = tempB.next;
+    BLength++;
+  }
+
+  if (ALength > BLength) {
+    for (let i = 0; i < ALength - BLength+1; i++) {
+      headA = headA.next;
+    }
+  } else {
+    for (let i = 0; i < BLength - ALength+1; i++) {
+      headB = headB.next;
+    }
+  }
+
+  while (headA && headB) {
+    if (headA === headB) {
+      return headA;
+    }
+    headA = headA.next;
+    headB = headB.next;
+  }
+  return null;
+};
