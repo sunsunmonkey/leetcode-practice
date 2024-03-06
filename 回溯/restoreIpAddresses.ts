3af807223a9c6316a6f71788eb14ea65758b98b1
sunsunmonkey <2933201543@qq.com>
2024-03-06 10:30:17 +0800

    fix: 3.5

--- 回溯/restoreIpAddresses.ts
+++ 回溯/restoreIpAddresses.ts
@@ -4,31 +4,36 @@ function restoreIpAddresses(s: string): string[] {
 
   function backTrace(startIndex: number) {
     if (helper.length === 4 && startIndex === s.length) {
+      console.log("11", helper);
       resArr.push(helper.join("."));
       return;
     }
 
     for (let i = startIndex; i < Math.min(s.length, startIndex + 3); i++) {
       const str = s.slice(startIndex, i + 1);
-
       if (isValid(str)) {
         helper.push(str);
       } else {
         continue;
       }
-
       backTrace(i + 1);
       helper.pop();
     }
   }
 
   backTrace(0);
-
+  console.log("res", resArr);
   return resArr;
 }
 
 function isValid(s: string): boolean {
-  if ((s.startsWith("0") && s.length > 1) || s > "255" || s.length === 0 || s.length>3) {
+  const num = Number(s);
+  if (
+    (s.startsWith("0") && s.length > 1) ||
+    num > 255 ||
+    s.length === 0 ||
+    s.length > 3
+  ) {
     return false;
   }
   return true;
