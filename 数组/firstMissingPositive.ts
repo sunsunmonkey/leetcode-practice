839ab4be7eeef3f894d47f8f97c9278669724356
sunsunmonkey <2933201543@qq.com>
2024-03-13 21:51:09 +0800

    feat: 3.13

--- 数组/firstMissingPositive.ts
+++ 数组/firstMissingPositive.ts
@@ -1,4 +1,4 @@
-function firstMissingPositive(nums: number[]): number {
+function firstMissingPositiveII(nums: number[]): number {
   let res = 1;
 
   nums.sort((a, b) => a - b);
@@ -11,3 +11,26 @@ function firstMissingPositive(nums: number[]): number {
 
   return res;
 }
+
+function firstMissingPositive(nums: number[]): number {
+  let len = nums.length;
+  let i = 0;
+
+  while (i < len) {
+    let num = nums[i];
+    if (num > 0 && nums[num - 1] !== num && num <= len) {
+      swap(nums, i, num - 1);
+    } else {
+      i++;
+    }
+  }
+  for (let i = 0; i < len; i++) {
+    if (nums[i] !== i + 1) {
+      return i + 1;
+    }
+  }
+  return len + 1;
+}
+const swap = (nums, i, j) => {
+  [nums[i], nums[j]] = [nums[j], nums[i]];
+};
